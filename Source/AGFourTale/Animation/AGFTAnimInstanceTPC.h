e6c69b3d7f4cc87ecc51587964acaa89a49d6a37
Andrei Gribanov <AndreiGrb@yandex.ru>
2024-03-15 23:26:40 +0100

    Created cpp files for TP character
    Here I will move BP variables to AnimProxy

---
+++ Source/AGFourTale/Animation/AGFTAnimInstanceTPC.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "AGFTAnimInstanceTPC.generated.h"
+
+
+UCLASS()
+class AGFOURTALE_API UAGFTAnimInstanceTPC : public UAnimInstance
+{
+	GENERATED_BODY()
+};
