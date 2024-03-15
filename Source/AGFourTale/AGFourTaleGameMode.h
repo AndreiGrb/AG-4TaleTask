de377a144013907c45bab53c19b8b31f4d077327
Andrei Gribanov <AndreiGrb@yandex.ru>
2024-03-15 21:40:43 +0100

    Initial project commit

---
+++ Source/AGFourTale/AGFourTaleGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AGFourTaleGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AAGFourTaleGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AAGFourTaleGameMode();
+};
+
+
+
