0b3c67ff27d03cd388e423ee9f4c8f9afd2c8b95
Andrei Gribanov <AndreiGrb@yandex.ru>
2024-03-15 23:54:42 +0100

    Implemented AnimProxy

--- Source/AGFourTale/Animation/AGFTAnimInstanceTPC.cpp
+++ Source/AGFourTale/Animation/AGFTAnimInstanceTPC.cpp
@@ -1,1 +1,50 @@
 ﻿#include "AGFTAnimInstanceTPC.h"
+
+#include "GameFramework/Character.h"
+
+
+void UAGFTAnimInstanceTPC::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+	
+	if (auto Character = Cast<ACharacter>(GetOwningActor()))
+	{
+		CharacterOwner = Character;
+	}
+}
+
+void FAGFTAnimInstanceTPC_Proxy::PreUpdate(UAnimInstance* InAnimInstance, float DeltaSeconds)
+{
+	FAnimInstanceProxy::PreUpdate(InAnimInstance, DeltaSeconds);
+
+	const auto AnimInstance = Cast<UAGFTAnimInstanceTPC>(InAnimInstance);
+	if (IsValid(AnimInstance->CharacterOwner))
+	{
+		UpdateValuesFromAnimInstance(AnimInstance);
+	}
+	else
+	{
+		bShouldUpdate = false;
+	}
+	
+	FAnimInstanceProxy::PreUpdate(InAnimInstance, DeltaSeconds);
+}
+
+void FAGFTAnimInstanceTPC_Proxy::UpdateValuesFromAnimInstance(const UAGFTAnimInstanceTPC* InAnimInstance)
+{
+}
+
+void FAGFTAnimInstanceTPC_Proxy::Update(float DeltaSeconds)
+{
+	CalculateValuesInProxy(DeltaSeconds);
+	
+	FAnimInstanceProxy::Update(DeltaSeconds);
+}
+
+void FAGFTAnimInstanceTPC_Proxy::CalculateValuesInProxy(const float DeltaSeconds)
+{
+	if (!bShouldUpdate)
+	{
+		return;
+	}
+}
--- Source/AGFourTale/Animation/AGFTAnimInstanceTPC.h
+++ Source/AGFourTale/Animation/AGFTAnimInstanceTPC.h
@@ -2,11 +2,57 @@
 
 #include "CoreMinimal.h"
 #include "Animation/AnimInstance.h"
+#include "Animation/AnimInstanceProxy.h"
 #include "AGFTAnimInstanceTPC.generated.h"
 
+
+class UCharacterMovementComponent;
+
+USTRUCT(BlueprintType)
+struct FAGFTAnimInstanceTPC_Proxy : public FAnimInstanceProxy
+{
+	GENERATED_BODY()
+
+public:
+	FAGFTAnimInstanceTPC_Proxy() {}
+	FAGFTAnimInstanceTPC_Proxy(UAnimInstance* Instance) {}
+
+private:
+	virtual void PreUpdate(UAnimInstance* InAnimInstance, float DeltaSeconds) override;
+	void UpdateValuesFromAnimInstance(const class UAGFTAnimInstanceTPC* InAnimInstance);
+
+#pragma region PreUpdate variables (directly from AnimInstance)
+	
+#pragma endregion
+	
+	virtual void Update(float DeltaSeconds) override;
+	void CalculateValuesInProxy(const float DeltaSeconds);
+
+	bool bShouldUpdate = true;
+
+#pragma region Update variables (in anim thread)
+	
+#pragma endregion
+};
 
 UCLASS()
 class AGFOURTALE_API UAGFTAnimInstanceTPC : public UAnimInstance
 {
 	GENERATED_BODY()
+
+private:
+	virtual void NativeInitializeAnimation() override;
+
+	
+	UPROPERTY(Transient, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	FAGFTAnimInstanceTPC_Proxy Proxy;
+
+	virtual FAnimInstanceProxy* CreateAnimInstanceProxy() override { return &Proxy; }
+	virtual void DestroyAnimInstanceProxy(FAnimInstanceProxy* InProxy) override {}
+
+	friend struct FAGFTAnimInstanceTPC_Proxy;
+
+
+	UPROPERTY(Transient, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<ACharacter> CharacterOwner;
 };
