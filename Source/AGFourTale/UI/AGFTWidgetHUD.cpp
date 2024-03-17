980c004a9024d940425b15e7ed473618624f2d44
Andrei Gribanov <AndreiGrb@yandex.ru>
2024-03-17 19:06:46 +0100

    Tested moving crosshair dynamically on screen

--- Source/AGFourTale/UI/AGFTWidgetHUD.cpp
+++ Source/AGFourTale/UI/AGFTWidgetHUD.cpp
@@ -1,1 +1,21 @@
 ﻿#include "AGFTWidgetHUD.h"
+
+#include "Components/CanvasPanel.h"
+#include "Components/CanvasPanelSlot.h"
+#include "Components/TextBlock.h"
+
+void UAGFTWidgetHUD::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	if (!TopCanvas || !TestCrosshair)
+	{
+		UE_LOG(LogTemp, Error, TEXT("[UAGFTWidgetHUD::NativeConstruct] TopCanvas || TestCrosshair == nullptr"));
+		return;
+	}
+	
+	if(UCanvasPanelSlot* PanelSlot = Cast<UCanvasPanelSlot>(TestCrosshair->Slot))
+	{
+		PanelSlot->SetPosition(FVector2D(0, 0));  // Sets the position to coordinates (100, 100)
+	}
+}
--- Source/AGFourTale/UI/AGFTWidgetHUD.h
+++ Source/AGFourTale/UI/AGFTWidgetHUD.h
@@ -5,8 +5,20 @@
 #include "AGFTWidgetHUD.generated.h"
 
 
+class UTextBlock;
+class UCanvasPanel;
+
 UCLASS()
 class AGFOURTALE_API UAGFTWidgetHUD : public UUserWidget
 {
 	GENERATED_BODY()
+
+private:
+	virtual void NativeConstruct() override;
+
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidget = "true", AllowPrivateAccess = "true"))
+	TObjectPtr<UCanvasPanel> TopCanvas;
+
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidget = "true", AllowPrivateAccess = "true"))
+	TObjectPtr<UTextBlock> TestCrosshair;
 };
