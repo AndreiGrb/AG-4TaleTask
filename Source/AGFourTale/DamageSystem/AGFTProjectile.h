c83c84920428ab129618325c4ab96449f6230ead
Andrei Gribanov <AndreiGrb@yandex.ru>
2024-03-17 13:50:55 +0100

    Created weapon and projectile cpp files

---
+++ Source/AGFourTale/DamageSystem/AGFTProjectile.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "AGFTProjectile.generated.h"
+
+UCLASS()
+class AGFOURTALE_API AAGFTProjectile : public AActor
+{
+	GENERATED_BODY()
+
+
+};
---
+++ Source/AGFourTale/DamageSystem/AGFTWeapon.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "AGFTWeapon.generated.h"
+
+UCLASS()
+class AGFOURTALE_API AAGFTWeapon : public AActor
+{
+	GENERATED_BODY()
+
+
+};
