7f29ba82f00e8464fa43cebe26a5d56001269993
Andrei Gribanov <AndreiGrb@yandex.ru>
2024-03-17 18:24:44 +0100

    Created HUD class to handle widgets

---
+++ Source/AGFourTale/Framework/AGFTHUD.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "AGFTHUD.generated.h"
+
+
+UCLASS()
+class AGFOURTALE_API AAGFTHUD : public AHUD
+{
+	GENERATED_BODY()
+
+	
+};
