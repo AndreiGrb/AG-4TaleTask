06695883ccd63eb16f64f65395d126b2f877109a
Andrei Gribanov <AndreiGrb@yandex.ru>
2024-03-17 13:52:49 +0100

    Created PlayerController class

---
+++ Source/AGFourTale/Framework/AGFTPlayerController.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "AGFTPlayerController.generated.h"
+
+
+UCLASS()
+class AGFOURTALE_API AAGFTPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+};
