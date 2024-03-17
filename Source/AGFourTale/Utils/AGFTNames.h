2b94381c8fc85346a915812c01336e41a2e4029a
Andrei Gribanov <AndreiGrb@yandex.ru>
2024-03-17 23:05:14 +0100

    Made shooting through muzzle socket and moved it

--- Source/AGFourTale/Utils/AGFTNames.h
+++ Source/AGFourTale/Utils/AGFTNames.h
@@ -1,6 +1,6 @@
 #pragma once
 
 #define SOCKETNAME_WEAPON_ATTACHMENT "WeaponAttachment"
-#define SOCKETNAME_WEAPON_SHOOT "Ammo"
+#define SOCKETNAME_WEAPON_SHOOT "Muzzle"
 
 #define COLLISION_PROFILENAME_PROJECTILE "Projectile"
