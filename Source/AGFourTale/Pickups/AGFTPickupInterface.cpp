81653772695321be299f0bc8a4cd1d8642961b3c
Andrei Gribanov <AndreiGrb@yandex.ru>
2024-03-19 01:15:32 +0100

    Added interface for pickups

---
+++ Source/AGFourTale/Pickups/AGFTPickupInterface.cpp
@@ -0,0 +1,7 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AGFTPickupInterface.h"
+
+
+// Add default functionality here for any IAGFTPickupInterface functions that are not pure virtual.
