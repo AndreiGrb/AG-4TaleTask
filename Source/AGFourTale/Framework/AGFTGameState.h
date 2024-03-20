72e005a83dde0ecf33faf096ba58187b7f733854
Andrei Gribanov <AndreiGrb@yandex.ru>
2024-03-20 09:11:17 +0100

    Added GameState and PlayerState classes

---
+++ Source/AGFourTale/Framework/AGFTGameState.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "AGFTGameState.generated.h"
+
+
+UCLASS()
+class AGFOURTALE_API AAGFTGameState : public AGameStateBase
+{
+	GENERATED_BODY()
+};
---
+++ Source/AGFourTale/Framework/AGFTPlayerState.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "AGFTPlayerState.generated.h"
+
+
+UCLASS()
+class AGFOURTALE_API AAGFTPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+};
