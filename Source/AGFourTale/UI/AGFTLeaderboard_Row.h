a83156dc03274be2af1c7c69debd9734386a9a6a
Andrei Gribanov <AndreiGrb@yandex.ru>
2024-03-20 10:27:58 +0100

    Added leaderboard widgets

---
+++ Source/AGFourTale/UI/AGFTLeaderboard_Row.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "AGFTLeaderboard_Row.generated.h"
+
+
+UCLASS()
+class AGFOURTALE_API UAGFTLeaderboard_Row : public UUserWidget
+{
+	GENERATED_BODY()
+};
